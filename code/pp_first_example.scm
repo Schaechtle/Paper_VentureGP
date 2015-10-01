079f868a7354ec30f7cabd4fb0e3aa504946efda
Schaechtle <ulrich.schaechtle@cs.rhul.ac.uk>
2015-10-01 12:36:15 +0100

    code 2 venture script and changed style similar to the GPGP paper / SAaeedi et al. 2014

--- code/pp_first_example.scm
+++ code/pp_first_example.scm
@@ -1,17 +1,25 @@
-(assume rain (flip 0.3))
+assume rain = flip(( 0.3))
 
-(assume sprinkler (if rain
-                    false
-                    (flip 0.8)))
+assume sprinkler = if( rain) {
+		      false }
+                   else {
+		      flip( 0.8) }
+                   
 
-(assume grass_wet (flip (if rain
-                          (if sprinkler
-                            0.99
-                            0.95)
-                          (if sprinkler
-                            0.6
-                            0.1))))
-
-(assume humidity (if rain
-                   (normal 95 2)
-                   (normal 50 20)))
+assume grass_wet = flip( if( rain) {
+			    if( sprinkler) {
+				0.99 }
+                            else {
+				0.95 }
+                         }
+                         else {
+			    if( sprinkler) {
+				0.6 }
+                            else {
+				0.1 }
+                         }
+		  )
+assume humidity = if( rain) {
+		      normal( 95 2)}
+                  else {
+		      normal( 50 20)}
