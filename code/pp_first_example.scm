ada24c542d12a0451d40cdbda45277f936b5078a
Schaechtle <ulrich.schaechtle@cs.rhul.ac.uk>
2015-10-06 18:19:24 +0100

    removed jargon from code, changed its presentation a bit

--- code/pp_first_example.scm
+++ code/pp_first_example.scm
@@ -1,25 +1,9 @@
-assume rain = flip(( 0.3))
-
-assume sprinkler = if( rain) {
-		      false }
-                   else {
-		      flip( 0.8) }
-                   
-
-assume grass_wet = flip( if( rain) {
-			    if( sprinkler) {
-				0.99 }
-                            else {
-				0.95 }
-                         }
-                         else {
-			    if( sprinkler) {
-				0.6 }
-                            else {
-				0.1 }
-                         }
-		  )
-assume humidity = if( rain) {
-		      normal( 95 2)}
-                  else {
-		      normal( 50 20)}
+#\linenumber{1}#assume rain = bernoulli( 0.3)
+#\linenumber{2}#assume sprinkler = if( rain) { false }
+#\linenumber{3}#                   else      {bernoulli( 0.8) }                   
+#\linenumber{4}#assume grass_wet = bernoulli( if( rain) { if( sprinkler) { 0.99 }  
+#\linenumber{5}#				     else           { 0.95 } }
+#\linenumber{6}#                         else {      if( sprinkler) { 0.6 } 
+#\linenumber{7}#                                     else         { 0.1 } } )
+#\linenumber{8}#assume humidity = if( rain) { normal( 95 2)}
+#\linenumber{9}#                  else { normal( 50 20) }
