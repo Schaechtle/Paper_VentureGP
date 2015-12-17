63c7f3c457edac4071541774956dc089a8ae1bf9
Schaechtle <ulrich.schaechtle@cs.rhul.ac.uk>
2015-12-17 10:21:25 +0000

    typos, merging paragraphs, removal of tscolor toolbox

--- code/pp_first_example_v2.scm
+++
@@ -1,9 +0,0 @@
-assume rain = flip(( 0.3))
-assume sprinkler = if( rain) { false }
-                   else      {flip( 0.8) }                   
-assume grass_wet = flip( if( rain) { if( sprinkler) { 0.99 }  
-				     else           { 0.95 } }
-                         else {      if( sprinkler) { 0.6 } 
-                                     else         { 0.1 } } )
-assume humidity = if( rain) { normal( 95 2)}
-                  else { normal( 50 20) }
